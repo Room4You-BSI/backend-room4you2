1938d72c3492a7e5d00f1b52f0d1b088da9b9027
VictorMomente <45798480+VictorMomente@users.noreply.github.com>
2020-06-22 03:10:41 -0300

    [Victor] - JWT
    Desenvolvendo sistema de login com JWT

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,24 @@
 import os.path
 from flask import Flask
-from flask_login import LoginManager
+from flask_jwt_simple import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder='templates')
+app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'dataBase.db')
-app.config['SECRET_KEY'] = 'secret'
+app.config['JWT_SECRET_KEY'] = 'dKorz3YmAdJpiMfM7_9b8kOYd7NhVslSZ4DoSwXkHYb9i0xSA7NC0WTYfQTULJZcYGY8qQ4I4OFbw7Z0nL3Y9JCv74SI9urpxdwpsfrJIonEO1cVcnlxpt1NW1QmwD6FY4b-jgGDdDYjIFF1sFBogj-kDeIbx-TW77oatX44PsQOH_mmCTxpKRRQdKIo7ER2o5r94X_OBqnwJTZziJRu6cdB887-DKDp1ItSFWFN4TLx8YTm22nDSmugyjB-K_IF9PARVlZrfUmj4IWnU-nQsQdrDeHx63PGs7dEHPx9qhOqr8dUVnrrVwVCF6vPJtySB0Ehq6qw'
 
-login_manager = LoginManager()
-login_manager.init_app(app) 
+jwt = JWTManager(app)
 db = SQLAlchemy(app)
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    header['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
+    return response
 
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import request, redirect, url_for, Response, abort
-from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db
 from app.models import User
+from flask_jwt_simple import create_jwt, jwt_required, get_jwt_identity
 
 from json import dumps
 from http import HTTPStatus
@@ -10,21 +10,27 @@ from http.client import HTTPException
 
 @app.route("/add_user", methods=["GET", "POST"])
 def register():
-    
+    """Register a user in the database."""
     if request.method == 'POST':
         try:
-            name = request.form["name"]
+            name = request.form["nome"]
             email = request.form["email"]
             pwd = request.form["password"]
+
+            existing_user = User.query.filter_by(email=email).first()
+
+            if existing_user:
+                return Response(dumps({"message": "USER ALREADY EXISTS"}), status=422, mimetype="application/json")
 
             user = User(name, pwd, email)
             db.session.add(user)
             db.session.commit()
 
             return Response(dumps({"message": "SUCCESS"}), status=200, mimetype="application/json")
+
         except HTTPException as e:
             return Response(dumps({"message": str(e)}), status=500, mimetype="application/json")
-        
+    
     return Response(dumps({"message": "NOT AVAILABLE"}), status=403, mimetype="application/json")
 
 @app.route("/get_profile", methods=["GET", "POST"])
@@ -37,22 +43,18 @@ def login():
 
             user = User.query.filter_by(email=email).first()
             
-            if not user or not user.verify_password(pwd):
-                return redirect(url_for("login"))
-
-            login_user(user)
+            if not user:
+                return Response(dumps({"message": "USER NOT FOUND"}), status=404, mimetype="application/json")
             
-            return Response(dumps([{"message": "SUCCESS"}]), status=200, mimetype="application/json")
+            if not user.verify_password(pwd):
+                return Response(dumps({"message": "WRONG PASSWORD"}), status=422, mimetype="application/json")
+
+            return Response(dumps({"message": "SUCCESS", "jwt": create_jwt(identity=email)}), status=200, mimetype="application/json")
+
         except HTTPException as e:
-            return Response(dumps([{"message": str(e)}]), status=500, mimetype="application/json")
-
-    return Response(dumps([{"message": "NOT AVAILABLE"}]), status=403, mimetype="application/json")
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return Response(dumps([{"message": "SUCCESS"}]), status=200, mimetype="application/json")
+            return Response(dumps({"message": str(e)}), status=500, mimetype="application/json")
+        
+    return Response(dumps({"message": "NOT AVAILABLE"}), status=403, mimetype="application/json")
 
 @app.route('/home', methods=["GET"])
 @app.route('/', methods=["GET"])
@@ -64,35 +66,77 @@ posts = [
     { 
         'title': 'Quarto São Carlos',
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'image': '',
-        'price': 10,
-        'rate': 50,
+        'image': 'https://media.gazetadopovo.com.br/haus/2018/10/quarto-infantil-dia-da-crianca-decoracao-design-arquitetura-dicas-karina-korn-5-768x521-baa14530.jpg',
+        'price': 720.5,
+        'rate': 5,
         'distance': '4,0 km do centro',
         'favorite': False,
-        'attributesColumn1': 'OfferCardColumnItemModel[]',
-        'attributesColumn2': 'OfferCardColumnItemModel[]'
+        'attributesColumn1': [
+            {
+                'label': 'Vaga', 
+                'available': True
+            },
+            {
+                'label': 'Wi-fi', 
+                'available': True
+            },
+        ],
+        'attributesColumn2': [
+            {
+                'label': 'Suite', 
+                'available': False
+            }
+        ]
     },
     {
         'title': 'Quarto Rodoviária',
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'image': '',
-        'price': 10,
-        'rate': 50,
-        'distance': '4,0 km do centro',
+        'image': 'https://q-cf.bstatic.com/images/hotel/max1024x768/200/200710933.jpg',
+        'price': 380.7,
+        'rate': 3,
+        'distance': '2,8 km do centro',
         'favorite': False,
-        'attributesColumn1': 'OfferCardColumnItemModel[]',
-        'attributesColumn2': 'OfferCardColumnItemModel[]'
+        'attributesColumn1': [
+            {
+                'label': 'Vaga', 
+                'available': True
+            },
+            {
+                'label': 'Wi-fi', 
+                'available': True
+            },
+        ],
+        'attributesColumn2': [
+            {
+                'label': 'Suite', 
+                'available': True
+            }
+        ]
     },
     {
         'title': 'Quarto Embaré',
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'image': '',
-        'price': 10,
-        'rate': 50,
-        'distance': '4,0 km do centro',
+        'image': 'https://images.madeiramadeira.com.br/product/images/75801354-quarto-de-solteiro-completo-com-guarda-roupa-closet-painel-cabeceira-e-nicho-mov-siena-moveis-1_zoom-1500x1500.jpg',
+        'price': 450.5,
+        'rate': 5,
+        'distance': '3,8 km do centro',
         'favorite': False,
-        'attributesColumn1': 'OfferCardColumnItemModel[]',
-        'attributesColumn2': 'OfferCardColumnItemModel[]'
+        'attributesColumn1': [
+            {
+                'label': 'Vaga', 
+                'available': True
+            },
+            {
+                'label': 'Wi-fi', 
+                'available': True
+            },
+        ],
+        'attributesColumn2': [
+            {
+                'label': 'Suite', 
+                'available': True
+            }
+        ]
     }
 ]
 
